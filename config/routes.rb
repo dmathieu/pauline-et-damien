9e77fa29457d60f5d50bb11d79a4d092cd9ede8a
Damien Mathieu <damien@heroku.com>
2016-12-10 16:02:05 -0800

    start adding the accomodations page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
+  get '/logement' => 'welcome#logement'
   root 'welcome#index'
 end
--- spec/requests/welcomes_spec.rb
+++ spec/requests/welcomes_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "Main page", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /logement" do
+    it "renders the accomodations page" do
+      get logement_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end

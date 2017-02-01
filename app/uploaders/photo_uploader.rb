4e7568c077a51b975e77a97efbcf3716ed3e4f7c
dmathieu <42@dmathieu.com>
2017-02-01 17:39:32 +0100

    generate and use thumb and large photos

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,6 +13,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
+
+  version :large do
+    process resize_to_fill: [400,400]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:

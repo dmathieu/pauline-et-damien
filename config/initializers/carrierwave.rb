795122810914636d9fbf2ad39847a08144d0e893
dmathieu <42@dmathieu.com>
2017-02-01 18:07:19 +0100

    don't configure google cloud if there's no credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/google'                        # required
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:     ENV["GOOGLE_STORAGE_ID"],
-    google_storage_secret_access_key: ENV["GOOGLE_STORAGE_SECRET"]
-  }
-  config.fog_directory = 'pauline-et-damien'
+if ENV["GOOGLE_STORAGE_ID"] && ENV["GOOGLE_STORAGE_SECRET"]
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/google'                        # required
+    config.fog_credentials = {
+      provider:                         'Google',
+      google_storage_access_key_id:     ENV["GOOGLE_STORAGE_ID"],
+      google_storage_secret_access_key: ENV["GOOGLE_STORAGE_SECRET"]
+    }
+    config.fog_directory = 'pauline-et-damien'
+  end
 end
